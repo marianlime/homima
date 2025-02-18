aa8e757a170b80209721379f62cc823f284fbd6e
Marian Chihai <2021529@brunel.ac.uk>
2025-02-18 18:22:06 +0000

    some changes to the navbar

--- frontend/homima/src/components/Navbar.tsx
+++ frontend/homima/src/components/Navbar.tsx
@@ -1,20 +1,24 @@
 import * as React from 'react'
-import { AppBar, Toolbar, Typography, Button} from '@mui/material';
+import { AppBar, Toolbar, Typography, Box, Button} from '@mui/material';
 import {Link } from '@tanstack/react-router'
 
 function Navbar() {
     return (
         <AppBar position="static">
-            <Toolbar>
-                <Typography variant="h6" sx={{flexGrow: 1}}>
-                    <Link  style={{ textDecoration: 'none', color: 'inherit'}} to="/">
-                    Homima
-                    </Link> 
+            
+                <Toolbar sx={{justifyContent: 'space-between'}}>
+                    <Typography variant="h5" noWrap>
+                        <Link to="/" style={{textDecoration: 'none', color: 'inherit'}}>
+                        Homima
+                        </Link>
                     </Typography>
-                <Button color="inherit" component={Link} to="/">Home</Button>
-                <Button color="inherit" component={Link} to="/create-room">Create a Room</Button>
-                <Button color="inherit" component={Link} to="/about">About</Button>
-            </Toolbar>
+                    <Box>
+                        <Button color="inherit" component={Link} to="/">Home</Button>
+                        <Button color="inherit" component={Link} to="/create-room">Create a room</Button>
+                        <Button color="inherit" component={Link} to="/about">About</Button>
+                    </Box>
+                </Toolbar>
+
         </AppBar>
     )
 }
--- frontend/homima/src/pages/AboutPage.tsx
+++ frontend/homima/src/pages/AboutPage.tsx
@@ -1,15 +1,21 @@
 // src/pages/AboutPage.tsx
 import React from 'react';
+import Navbar from '../components/Navbar'
 import { Typography } from '@mui/material';
 
 function AboutPage() {
   return (
+    <>
+    <div>
+      <Navbar/>
+    </div>
     <div style={{ padding: '20px' }}>
       <Typography variant="h4">About</Typography>
       <Typography variant="body1">
         This app allows you to create and join cinema rooms to watch videos together.
       </Typography>
     </div>
+    </>
   );
 }
 
